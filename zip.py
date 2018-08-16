a3bdc89fab46f4d9501a7d30e4eb8ab49266eef2
Samuel Martin <Samuel Martin>
2018-08-16 09:59:16 +0100

    added print-tree function

--- zip.py
+++ zip.py
@@ -6,18 +6,32 @@ import os
 import shutil
 import zipfile
 
-#  Setup
+#  Setup.
 
 os.makedirs('unzipped')
 
+#  Unzip document.
 zip_ref = zipfile.ZipFile('essay.docx', 'r')
 zip_ref.extractall('unzipped')
 zip_ref.close()
+
+def list_tree(path):
+    '''Taken from: https://stackoverflow.com/
+    questions/9727673/list-directory-tree-structure-in-python'''
+    for root, dirs, files in os.walk(path):
+        level = root.replace(path, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}/'.format(subindent, f)
+
+list_tree('unzipped')
 
 with open('unzipped/word/document.xml') as f:
     text = f.read()
 print(text)
 
-#  Pulldown
+#  Pulldown.
 
 shutil.rmtree('unzipped')  # Removes full folder.
