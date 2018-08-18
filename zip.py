cc56ee87233f5df0a0c7ff986e2a10f09eee5c26
Samuel Martin <Samuel Martin>
2018-08-18 18:10:23 +0100

    setup and takedown completed, now ready for experimentation

--- zip.py
+++ zip.py
@@ -8,6 +8,8 @@ import zipfile
 
 #  Setup.
 
+if 'unzipped' in os.listdir():
+    shutil.rmtree('unzipped')  # Removes any leftover folder from old crash.
 os.makedirs('unzipped')
 
 #  Unzip document.
@@ -15,22 +17,18 @@ zip_ref = zipfile.ZipFile('essay.docx', 'r')
 zip_ref.extractall('unzipped')
 zip_ref.close()
 
-def list_tree(path):
-    '''Taken from: https://stackoverflow.com/
-    questions/9727673/list-directory-tree-structure-in-python'''
-    for root, dirs, files in os.walk(path):
-        level = root.replace(path, '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            print('{}{}/'.format(subindent, f)
 
-list_tree('unzipped')
+#with open('unzipped/word/document.xml') as f:
+#    text = f.read()
+#print(text)
 
-with open('unzipped/word/document.xml') as f:
-    text = f.read()
-print(text)
+
+
+#  Rezip document
+shutil.make_archive('new_document', 'zip', 'unzipped')
+if 'new_document.docx' in os.listdir():
+    os.remove('new_document.docx')
+os.rename('new_document.zip', 'new_document.docx')
 
 #  Pulldown.
 
